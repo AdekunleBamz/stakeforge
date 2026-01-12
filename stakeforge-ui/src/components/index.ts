d3cc31f46f62041cfacd8c2fc49435d6d20bafa8
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-12 17:55:59 +0100

    feat(ui): export additional components

--- stakeforge-ui/src/components/index.ts
+++ stakeforge-ui/src/components/index.ts
@@ -2,3 +2,6 @@ export { Header } from './Header';
 export { StatsCard } from './StatsCard';
 export { MintSection } from './MintSection';
 export { StakeSection } from './StakeSection';
+export { Toast } from './Toast';
+export { LoadingSpinner } from './LoadingSpinner';
+export { RewardsPanel } from './RewardsPanel';
